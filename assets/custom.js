a83be22abaf681ff49646c9c368e11a3297d9ba1
Braden Cariaga <bradencariaga@hotmail.com>
2021-10-26 17:55:02 -0400

    Fixed about counters

--- assets/custom.js
+++ assets/custom.js
@@ -1,1 +1,15 @@
+/* Credits to https://gist.github.com/jjmu15 for the isInView method. */
+HTMLElement.prototype.isInView = function() {
+     var rect = this.getBoundingClientRect();
+     var html = document.documentElement;
+     return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || html.clientHeight) &&
+          rect.right <= (window.innerWidth || html.clientWidth)
+     );
+}
 
+function formatNumber(num) {
+     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
